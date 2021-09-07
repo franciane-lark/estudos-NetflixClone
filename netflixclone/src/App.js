e24c4622e461499980cbad4ab543f83ddcff9448
Franciane Lark <franciane.lark019@gmail.com>
2021-09-07 12:09:15 -0400

    API consumida com sucesso

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,  useState } from  'react';
 import Omdb from './Omdb';
 export default () => {
 
+    const [movielist, setMovieList] = useState([]);
+
     useEffect(() => {
-      const loadAll = async () =>{
+      const loadAll = async () => {
       //Pegando a lista TOTAL
       let list = await Omdb.getHomeList();
       console.log(list);
+      setMovieList(list);
+      loadAll();
       }
     }, []);
 
